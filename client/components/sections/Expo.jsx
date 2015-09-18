4448036fb54e202239cf95e248b1db512e270018
Kevin Chen <kevin.chen@veeva.com>
2015-09-18 09:49:12 -0400

    Finished up carousel

--- client/components/sections/Expo.jsx
+++ client/components/sections/Expo.jsx
@@ -44,13 +44,16 @@ Carousel = React.createClass({
             currentIndex: this.props.currentIndex
         };
     },
+    componentWillMount() {
+        this.resizeIntervals = {};
+    },
     render() {
         return(
             <div className="carousel">
                 <div className="panels">
                 {this.props.sources.map((source, index) => {
                     var isActive = (index === this.state.currentIndex) ? "active" : "";
-                    return <img className={["panel", isActive].join(" ")} key={index} src={source.src}/>;
+                    return <img className={["panel", isActive].join(" ")} key={index} src={source.src} data-index={index} />;
                 })}
                 </div>
                 <div className="overlay">
@@ -63,6 +66,44 @@ Carousel = React.createClass({
                 </div>
             </div>
         );
+    },
+    componentDidMount() {
+        var self = this;
+        var $el = $(this.getDOMNode());
+        var $container = $el.find(".panels");
+
+        $el.find(".panel").each(function() {
+            $(this).on("load", function() {
+                var $panel = $(this);
+                var key = $(this).data("reactid");
+                var index = $(this).data("index");
+
+                self.applyImageFit($container, $panel);
+                self.resizeIntervals[key] = setInterval($.proxy(function($container, $panel) {
+                    if (parseInt($panel.data("index"), 10) !== index) return;
+                    this.applyImageFit($container, $panel);
+                }, self, $container, $panel), 100);
+            });
+        });
+    },
+    componentWillUnmount() {
+        _.each(this.resizeIntervals, function(interval) {
+            clearInterval(interval);
+        });
+    },
+    applyImageFit($container, $panel) {
+        var fit = computeImageFit(
+            IMAGE_FIT.BEST_FILL,
+            IMAGE_FIT.ALIGN_CENTER,
+            $panel.width(), $panel.height(),
+            $container.width(), $container.height()
+        );
+        $panel.css({
+            left: fit.x,
+            top: fit.y,
+            width: fit.width,
+            height: fit.height
+        });
     },
     handleControlClick(index, ev) {
         if (this.state.currentIndex === index) return;
